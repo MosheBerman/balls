db9a65bc5785d1e1c644ef1d0a86b817cce62e0e
Moshe <moshberm@gmail.com>
2014-01-23 19:04:16 -0500

    Balls appear when you tap on the screen.

--- BallGame/BAGViewController.m
+++ BallGame/BAGViewController.m
@@ -8,11 +8,24 @@
 
 #import "BAGViewController.h"
 
+#import "BAGBall.h"
+
 @interface BAGViewController ()
+
+@property (nonatomic, strong) NSMutableArray *balls;
 
 @end
 
 @implementation BAGViewController
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        _balls = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
 
 - (void)viewDidLoad
 {
@@ -24,6 +37,50 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    for (UITouch *touch in touches)
+    {
+        
+        CGPoint location = [touch locationInView:self.view];
+        
+        if ([[self balls] count] == 0)
+        {
+            [self newBallAtPoint:location];
+        }
+        
+        
+        [self newBallAtPoint:location];
+        
+        
+    }
+    
+    
+}
+
+#pragma mark - New Ball
+
+- (void)newBallAtPoint:(CGPoint)point
+{
+    
+    CGFloat randomRadius = arc4random() % 50 + 15;
+    BAGBall *ball = [BAGBall ballWithRadius:randomRadius];
+    ball.center = point;
+    [[self balls] addObject:ball];
+    [[self view] addSubview:ball];
+    
 }
 
 @end
