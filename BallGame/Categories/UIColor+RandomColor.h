fe0901c6bdbd1610ba56d86bf804b797eb4741da
Moshe <moshberm@gmail.com>
2014-01-23 19:03:40 -0500

    Added random colors to the project.

---
+++ BallGame/Categories/UIColor+RandomColor.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+RandomColor.h
+//  BallGame
+//
+//  Created by Moshe Berman on 1/23/14.
+//  Copyright (c) 2014 Moshe Berman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@interface UIColor (RandomColor)
+
++ (UIColor *)randomColor;
+
+@end
---
+++ BallGame/Categories/UIColor+RandomColor.m
@@ -0,0 +1,23 @@
+//
+//  UIColor+RandomColor.m
+//  BallGame
+//
+//  Created by Moshe Berman on 1/23/14.
+//  Copyright (c) 2014 Moshe Berman. All rights reserved.
+//
+
+#import "UIColor+RandomColor.h"
+
+@implementation UIColor (RandomColor)
+
++ (UIColor *)randomColor
+{
+    
+    CGFloat red = (CGFloat)(arc4random() % 255 + 0)/255.0;
+    CGFloat green = (CGFloat)(arc4random() % 255 + 0)/255.0;
+    CGFloat blue = (CGFloat)(arc4random() % 255 + 0)/255.0;
+    
+    return [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
+}
+
+@end
